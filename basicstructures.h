67e9fbab6022746e0ba47e8011f8fee32aa0e8c5
klimkomx <klimkomx@gmail.com>
2020-05-06 15:03:23 +0300

    :thinking:

---
+++ basicstructures.h
@@ -0,0 +1,36 @@
+#ifndef TOADS_BATTLE_BASIC_STRUCTURES_H
+#define TOADS_BATTLE_BASIC_STRUCTURES_H
+
+class point
+{
+public:
+	int x, y;
+	point() {
+		x = y = 0;
+	}
+	point(int a, int b) {
+		x = a;
+		y = b;
+	}
+	~point();	
+};
+
+
+class step
+{
+public:
+	point beginPoint, endPoint;
+
+	step() {
+		beginPoint = endPoint = point();
+	}
+	step(point A, point B) {
+		beginPoint = A;
+		endPoint = B;
+	}
+
+	~step();
+	
+};
+
+#endif
---
+++ bots.h
@@ -0,0 +1,13 @@
+#ifndef TOADS_BATTLE_BOTS_H
+#define TOADS_BATTLE_BOTS_H
+
+class toadsBattleBots
+{
+public:
+	toadsBattleBots();
+	~toadsBattleBots();
+private:
+
+};
+
+#endif
