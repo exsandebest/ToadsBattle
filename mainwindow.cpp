7f60cbd52be3028a814ee2404b72d69fea3f3669
exsandebest <dan56213@mail.ru>
2020-05-08 14:10:45 +0300

    Added src, folders, started ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+int botLevel = 1;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->showMaximized();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +16,17 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_btn_level_clicked()
+{
+    if (botLevel == 1){
+        botLevel = 2;
+        ui->btn_level->setText("LEVEL: MEDIUM");
+    } else if (botLevel == 2){
+        botLevel = 3;
+        ui->btn_level->setText("LEVEL: HARD");
+    } else if (botLevel == 3){
+        botLevel = 1;
+        ui->btn_level->setText("LEVEL: EASY");
+    }
+}
