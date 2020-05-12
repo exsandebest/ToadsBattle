40c870fae3dac2062d943b96f8520e319e181b0b
klimkomx <klimkomx@gmail.com>
2020-05-12 17:48:06 +0300

    Updates

--- bots.cpp
+++ bots.cpp
@@ -1,9 +1,16 @@
 #include "bots.h"
+#include <algorithm>
 
 toadsBattleBots::toadsBattleBots(int tSize, int complexity, int number){
     tableSize = tSize;
     botComplexity = complexity;
     playerNumber = number;
+    if (complexity == 1)
+        this -> predictionFunction = &toadsBattleBots::firstLevelBotPredictionFunction;
+    else if (complexity == 2)
+        this -> predictionFunction = &toadsBattleBots::secondLevelBotPredictionFunction;
+    else
+        this -> predictionFunction = &toadsBattleBots::thirdLevelBotPredictionFunction;
 };
 
 toadsBattleBots::toadsBattleBots(){
@@ -12,10 +19,19 @@ toadsBattleBots::toadsBattleBots(){
     playerNumber = 2;
 };
 
-step toadsBattleBots::nextStep(const std::vector<std::vector<int> > &table){
+step toadsBattleBots::nextStep(const std::vector<std::vector<int> > &table) {
     std::vector<step> steps = getStepsPool(table, this -> playerNumber);
 
-    return steps[steps.size() - 1];
+    step queryResult = step(-1, -1, -1, -1);
+    double bestValue = -INF, tmpValue;
+    for (step stepToPredict : steps) {
+        tmpValue = (this ->*predictionFunction)(stepToPredict, table);
+        if (tmpValue > bestValue) {
+            bestValue = tmpValue;
+            queryResult = stepToPredict;
+        }
+    }
+    return queryResult;
 };
 
 std::vector<std::vector<bool> > toadsBattleBots::getControlTable(const std::vector<std::vector<int> >& table,const  int queryPlayerNumber) {
@@ -26,8 +42,8 @@ std::vector<std::vector<bool> > toadsBattleBots::getControlTable(const std::vect
     for (int i = 0; i < tableSize; ++i) {
         for (int j = 0; j < tableSize; ++j) {
             if (table[i][j] == queryPlayerNumber) {
-                for (int di = -2; di < 3 && i + di < tableSize && i + di >= 0; ++di) {
-                    for (int dj = -2; dj < 3 && j + dj < tableSize && j + dj >= 0; ++dj) {
+                for (int di = std::max(-2, -i); di < std::min(3, tableSize - i) ; ++di) {
+                    for (int dj = std::max(-2, -j); dj < std::min(3, tableSize - j); ++dj) {
                         if (table[i + di][j + dj] == emptyCell || table[i + di][j + dj] == queryPlayerNumber)
                             controlTable[i][j] = true;
                     }
@@ -38,14 +54,14 @@ std::vector<std::vector<bool> > toadsBattleBots::getControlTable(const std::vect
     return controlTable;
 }
 
-std::vector<step> toadsBattleBots::getStepsPool(const std::vector<std::vector<int> >& table,const int queryPlayerNumber) {
+std::vector<step> toadsBattleBots::getStepsPool(const std::vector<std::vector<int> >& table, const int queryPlayerNumber) {
     std::vector<step> queryResult;
 
     for (int i = 0; i < tableSize; ++i) {
         for (int j = 0; j < tableSize; ++j) {
             if (table[i][j] == queryPlayerNumber) {
-                for (int di = -2; di < 3; ++di) {
-                    for (int dj = -2; dj < 3; ++dj) {
+                for (int di = std::max(-2, -i); di < std::min(3, tableSize - i); ++di) {
+                    for (int dj = std::max(-2, -j); dj < std::min(3, tableSize - j); ++dj) {
                         if (table[i + di][j + dj] == emptyCell)
                             queryResult.push_back(step(i, j, i + di, j + dj));
                     }
@@ -56,8 +72,59 @@ std::vector<step> toadsBattleBots::getStepsPool(const std::vector<std::vector<in
     return queryResult;
 }
 
-std::vector<std::vector<int> > getAfterStepTable(const step playerStep, const std::vector<std::vector<int> >& table, const int queryPlayerNumber) {
+std::vector<std::vector<int> > toadsBattleBots::getAfterStepTable(const step& playerStep, const std::vector<std::vector<int> >& table, const int queryPlayerNumber) {
     std::vector<std::vector<int> > queryResult  = table;
+    int enemyPlayerNumber = (queryPlayerNumber + 1) % 2;
+    if (std::max(std::abs(playerStep.beginPoint.x - playerStep.endPoint.x), std::abs(playerStep.beginPoint.y - playerStep.endPoint.y)) == 2)
+        queryResult[playerStep.beginPoint.x][playerStep.beginPoint.y] = emptyCell;
+    queryResult[playerStep.endPoint.x][playerStep.endPoint.y] = queryPlayerNumber;
 
-    // if (abs())
+    for (int x = std::max(0, playerStep.endPoint.x - 1); x < std::min(tableSize, playerStep.endPoint.x + 2); ++x) {
+        for (int y = std::max(0, playerStep.endPoint.y - 1); y < std::min(tableSize, playerStep.endPoint.y + 2); ++y) {
+            if (queryResult[x][y] == enemyPlayerNumber)
+                queryResult[x][y] = queryPlayerNumber;
+        }
+    }
+
+    return queryResult;
+}
+
+double toadsBattleBots::countPlayerCells(const std::vector<std::vector<int> > &table, int queryPlayerNumber) {
+    int result = 0;
+    for (int i = 0; i < tableSize; ++i) {
+        for (int j = 0; j < tableSize; ++j) {
+            if (table[i][j] == queryPlayerNumber)
+                ++result;
+        }
+    }
+    return result;
+}
+
+inline double  toadsBattleBots::countPlayerControl(const std::vector<std::vector<int> > &table, int queryPlayerNumber) {
+    std::vector<std::vector<bool> > controlTable = getControlTable(table, queryPlayerNumber);
+    return countPlayerControl(controlTable, queryPlayerNumber);
+}
+
+double  toadsBattleBots::countPlayerControl(const std::vector<std::vector<bool> > &controlTable, int queryPlayerNumber) {
+    int result = 0;
+    for (int i = 0; i < tableSize; ++i) {
+        for (int j = 0; j < tableSize; ++j) {
+            if (controlTable[i][j])
+                ++result;
+        }
+    }
+    return result;
+}
+
+//predictions
+double toadsBattleBots::firstLevelBotPredictionFunction(const step& presumableStep, const std::vector<std::vector<int> > &table) {
+    return 0;
+}
+
+double toadsBattleBots::secondLevelBotPredictionFunction(const step& presumableStep, const std::vector<std::vector<int> > &table) {
+    return 0;
+}
+
+double toadsBattleBots::thirdLevelBotPredictionFunction(const step& presumableStep, const std::vector<std::vector<int> > &table) {
+    return 0;
 }
--- bots.h
+++ bots.h
@@ -22,23 +22,26 @@ private:
     //constants
     const double POTENTIAL_DANGER_FACTOR = 0.5;
     const double RANDOM_FACTOR = 0.01;
-
+    const double INF = 1e18;
+    const double EPS_MACHINE = 1e-7;
     //variables
     int tableSize, botComplexity, playerNumber;
+    double (toadsBattleBots::*predictionFunction)(const step&, const  std::vector<std::vector<int> >&);
 
     //get functions
-    std::vector<step> getStepsPool(const std::vector<std::vector<int> >& table, const int queryPlayerNumber);
-    std::vector<std::vector<bool> > getControlTable(const std::vector<std::vector<int> >& table,const  int queryPlayerNumber);
-    std::vector<std::vector<int> > getAfterStepTable(const step playerStep, const std::vector<std::vector<int> >& table);
+    std::vector<step> getStepsPool(const std::vector<std::vector<int> >& table,int queryPlayerNumber);
+    std::vector<std::vector<bool> > getControlTable(const std::vector<std::vector<int> >& table,int queryPlayerNumber);
+    std::vector<std::vector<int> > getAfterStepTable(const step& playerStep, const std::vector<std::vector<int> >& table, int queryPlayerNumber);
 
     //characteristic count functions
-    double countPlayerCells(const std::vector<std::vector<int> >& table);
-    double countPlayerControl(const std::vector<std::vector<int> >& table);
+    double countPlayerCells(const std::vector<std::vector<int> >& table, int queryPlayerNumber);
+    double countPlayerControl(const std::vector<std::vector<bool> >& controlTable, int queryPlayerNumber);
+    double countPlayerControl(const std::vector<std::vector<int> >& table, int queryPlayerNumber);
 
     //prediction functions
-    double firstLevelBotPredictionFunction(const step presumableStep, const std::vector<std::vector<int> >& table);
-    double secondLevelBotPredictionFunction(const step presumableStep, const std::vector<std::vector<int> >& table);
-    double thirdLevelBotPredictionFunction(const step presumableStep, const std::vector<std::vector<int> >& table);
+    double firstLevelBotPredictionFunction(const step& presumableStep, const std::vector<std::vector<int> >& table);
+    double secondLevelBotPredictionFunction(const step& presumableStep, const std::vector<std::vector<int> >& table);
+    double thirdLevelBotPredictionFunction(const step& presumableStep, const std::vector<std::vector<int> >& table);
 };
 
 #endif
